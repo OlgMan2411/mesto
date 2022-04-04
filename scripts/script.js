6ed543c38fddc03fe227dc9872bfbc2ae2e919a4
OlgMan2411 <omanaeva@yandex.ru>
2022-04-04 11:50:38 +0300

    feat: просмотр фото и настройка попапов

--- scripts/script.js
+++ scripts/script.js
@@ -1,115 +1,101 @@
-// Набор действий и переменных для работы с попапом. 
-// Сначала - редактирование имени и профессии профиля (где Кусто)
+// Элементы открывания попапов
+const openProfileButton = document.querySelector(".profile__edit-batton");
+const openAddCardButton = document.querySelector('.profile__add-batton');
 
-// Заголовки попапов:
-const popupFillers = [
-  {
-    popTitle: 'Редактировать профиль',    
-    popNamePlaceholder: 'Имя пользователя',
-    popInfoPlaceholder: 'профессия',
-  },
-  {
-    popTitle: 'Новое место',
-    popNamePlaceholder: 'Название',
-    popInfoPlaceholder: 'Ссылка на картинку',
-  },
-]
+// Закрывашки
+const closeProfileEdit = document.querySelector('.popup_type_profileEdit .popup__closed');
+const closeAddPhoto = document.querySelector('.popup_type_addCard .popup__closed');
+const closeViewPhoto = document.querySelector('.popup__closed_picview');
 
-// Чистый элемент попапа
-const blankPopup = document.querySelector('.popup');
+// Уже заполненные поля пользователя
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
 
-let openProfilePopup = document.querySelector(".profile__edit-batton");
+// Поля ввода профиля пользователя
+const profNameInput = document.querySelector('.popup_type_profileEdit .form__input_name');  
+const profJobInput = document.querySelector('.popup_type_profileEdit .form__input_profession');
 
+// Попап редактирования профиля
+const profilEditPop = document.querySelector('.popup_type_profileEdit');
 
-function profileOpener() {
-  profilePopup = document.querySelector('.popup');
-  console.log('Копия');
-  console.log(profilePopup);
-  const profileName = document.querySelector('.profile__name');
-  const profileProfession = document.querySelector('.profile__profession');
-  const profname = profilePopup.querySelector('.form__input_name-fild');  
-  const profFild = profilePopup.querySelector('.form__input_profession-link-fild');  
-  const closeProfPopup = profilePopup.querySelector(".popup__closed");
-  const profform = profilePopup.querySelector(".form");
-  
-  profilePopup.classList.toggle('popup_opened');
-  profilePopup.querySelector('.form__title').textContent = popupFillers[0].popTitle;
+// Попап открытия новой карточки с фото
+const newCardPopup = document.querySelector('.popup_type_addCard');
 
-  profname.name = popupFillers[0].popNamePlaceholder;  
-  profname.placeholder = popupFillers[0].popNamePlaceholder;  
-  profname.value = profileName.textContent;
-  
-  profFild.name = popupFillers[0].popInfoPlaceholder;
-  profFild.placeholder = popupFillers[0].popInfoPlaceholder;
-  profFild.value = profileProfession.textContent;  
-  
-  closeProfPopup.addEventListener('click', function() {
-    popupCloser(profilePopup);    
-  }
-  );
-  
-  profform.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    profileName.textContent = profname.value;
-    profileProfession.textContent = profFild.value;    
-    popupCloser(profilePopup);
-  }
-  );
+// Попап открытия просмотра фото
+const picviewPopup = document.querySelector('.popup_type_picview');
+
+// Поля ввода новой карточки с фото
+const addCardfNameInput = newCardPopup.querySelector('.popup_type_addCard .form__input_name');  
+const addCardLinkInput = newCardPopup.querySelector('.popup_type_addCard .form__input_profession');
+
+// Куда шаблон новой карточки и куда ее вставляем
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements'); 
+
+// Для слушателя кнопки сохранения профиля
+const profileFormSave = document.querySelector('.popup_type_profileEdit .form__saved');
+
+// Для слушателя кнопки сохранения профиля
+const newCardSave = newCardPopup.querySelector('.popup_type_addCard .form__saved');
+
+function openEditProfilePopup() {
+  profilEditPop.classList.toggle('popup_opened');
+  profNameInput.value = profileName.textContent;
+  profJobInput.value = profileProfession.textContent;
 };
 
-function popupCloser(elementToClose) {
-  elementToClose.classList.toggle('popup_opened');  
-  // elementToClose.replaceWith(blankPopup);
+function openPopup(popup) {
+  popup.classList.toggle('popup_opened');
 };
 
-function popupPicCloser(elementToClose) {
-  elementToClose.classList.toggle('popup-picview_opened');
+function closePopup(popup) {
+  popup.classList.toggle('popup_opened');
 };
 
-openProfilePopup.addEventListener('click', profileOpener);
-
-
-
-// Запуск попапа с добавлением карточки
-
-let openAddCardPopup = document.querySelector('.profile__add-batton');
-
-function addCardOpener() {
-  addCardPopup = document.querySelector('.popup');
-
-  const cardsContainer = document.querySelector('.elements');
-  const nameFild = addCardPopup.querySelector('.form__input_name-fild');
-  const profLinkFild = addCardPopup.querySelector('.form__input_profession-link-fild');
-  const closePopup = addCardPopup.querySelector(".popup__closed");
-  const form = addCardPopup.querySelector(".form");
-  
-  addCardPopup.classList.toggle('popup_opened'); //querySelector(".popup").  
-  addCardPopup.querySelector('.form__title').textContent = popupFillers[1].popTitle;
-
-  nameFild.classList.add('form__input_grey-placehold');
-  nameFild.name = popupFillers[1].popNamePlaceholder;  
-  nameFild.placeholder = popupFillers[1].popNamePlaceholder;    
-  
-  profLinkFild.classList.add('form__input_grey-placehold');
-  profLinkFild.name = popupFillers[1].popInfoPlaceholder;
-  profLinkFild.placeholder = popupFillers[1].popInfoPlaceholder;  
-  
-  closePopup.addEventListener('click', function() {
-    popupCloser(addCardPopup);
-  }
-  );
-  
-  form.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    
-    newCard = makeCard(profLinkFild.value, nameFild.value);    
-    cardsContainer.prepend(newCard);    
-    popupCloser(addCardPopup);  
-  }
-  );
+function closeAddPhotoPopup() {
+  newCardPopup.classList.toggle('popup_opened');
+  addCardfNameInput.value = '';
+  addCardLinkInput.value = '';
 };
 
-openAddCardPopup.addEventListener('click', addCardOpener);
+function profileFormSubmitHandler (evt) {
+  evt.preventDefault(); 
+  profileName.textContent = profNameInput.value;
+  profileProfession.textContent = profJobInput.value;  
+  closePopup(profilEditPop)    
+};
+
+function addCardFormSubmitHandler (evt) {
+  evt.preventDefault(); 
+  elements.prepend(makeCard(addCardLinkInput.value, addCardfNameInput.value));    
+  closePopup(newCardPopup);
+};
+
+// Слушатели попапа редактирования профиля
+openProfileButton.addEventListener('click', () => {openEditProfilePopup()});
+closeProfileEdit.addEventListener('click', () => {closePopup(profilEditPop)});
+profileFormSave.addEventListener('click', profileFormSubmitHandler);
+
+// Слушатели попапа ввода новой фотки
+openAddCardButton.addEventListener('click', () => {openPopup(newCardPopup)});
+closeAddPhoto.addEventListener('click', () => {closeAddPhotoPopup()});
+newCardSave.addEventListener('click', addCardFormSubmitHandler);
+
+
+// Вызов окна просмотра фотографии
+const openPicviewPopup = (link, name) => {  
+  picviewPopup.querySelector('.popup__photo').src = link;
+  picviewPopup.querySelector('.popup__title').textContent = name;
+  picviewPopup.querySelector('.popup__photo').alt = name;  
+  openPopup(picviewPopup);
+};
+
+closeViewPhoto.addEventListener('click', () => {closePopup(picviewPopup)});
+// closePicPopup.addEventListener('click', function() {
+//   popupPicCloser(picviewElements);
+// }
+// );
+
 
 
 
@@ -139,10 +125,6 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
   ];
-
-
-const elementTemplate = document.querySelector('#element-template').content;
-const elements = document.querySelector('.elements'); 
 
 const makeCard = (link, name) => {
   // клонируем содержимое тега template  
@@ -174,18 +156,3 @@ initialCards.forEach((item) => {
   let newCard = makeCard(item.link, item.name);  
   elements.append(newCard);  
 });
-
-// Вызов окна просмотра фотографии
-const openPicviewPopup = (link, name) => {  
-  const picviewElements = document.querySelector('.popup-picview');
-  const closePicPopup = picviewElements.querySelector(".popup-picview__closed");
-  picviewElements.querySelector('.popup-picview__photo').src = link;
-  picviewElements.querySelector('.popup-picview__title').textContent = name;
-  picviewElements.classList.toggle('popup-picview_opened');
-};
-
-
-closePicPopup.addEventListener('click', function() {
-  popupPicCloser(picviewElements);
-}
-);
