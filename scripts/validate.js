47581bea624cd036817f31af2eb174d975143dac
OlgMan2411 <omanaeva@yandex.ru>
2022-04-21 23:44:54 +0300

    Готово к первой проверке

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,144 +1,68 @@
-const UserForm = document.forms.editUser
-const CardForm = document.forms.addCard
-
 const validClasses = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__button',
-  inactiveButtonClass: 'popup__button_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  editUserId: 'editUser',
+  addCardID: 'addCard',
+  saveFormClass: '.form__save',
+  activeSubmitButtonClass: 'form__save_active'
 }; 
 
-
 // Включить или выключить кнопку сохранить
-const setSaveButtonState = (buttonElement, isValid) => {
+const setSaveButtonState = (buttonElement, isValid, activeSubmitButtonClass) => {
   if (isValid) {
-    buttonElement.classList.add('form__save_active');
+    buttonElement.classList.add(activeSubmitButtonClass);
   } else {
-    buttonElement.classList.remove('form__save_active');
+    buttonElement.classList.remove(activeSubmitButtonClass);
   }
 };
 
-const validateInput = (input) => {
-  // console.log(input.parentNode.querySelector('.user-name-error'));  
+const validateInput = (input) => {  
   const errorElement = input.parentNode.querySelector(`.${input.id}-error`);
   errorElement.textContent = '';
   errorElement.textContent = input.validationMessage;  
 };
 
-const handleSubmit = (event) => {
+const handleSubmit = (event, handler) => {
   event.preventDefault();  
-  const currentForm = event.target;  
+  const currentForm = event.target;
 
   if (currentForm.checkValidity()) {    
+    handler();
     currentForm.reset();
   }
 };
 
-const handleInput = (event) => {
+const handleInput = (event, buttonElement, activeSubmitButtonClass) => {
   const currentForm = event.currentTarget;
-  const input = event.target;
-  const buttonElement = currentForm.querySelector('.form__save')
+  const input = event.target;  
 
   validateInput(input);
-  setSaveButtonState(buttonElement, currentForm.checkValidity())
+  setSaveButtonState(buttonElement, currentForm.checkValidity(), activeSubmitButtonClass)
   
 };
 
-// В ПРОЦЕССЕ СВЕДЕНИЯ в ЕДИНУЮ функцю: вынести в переменную checkValidity
-
-UserForm.addEventListener('submit', handleSubmit);
-CardForm.addEventListener('submit', handleSubmit);
-
-UserForm.addEventListener('input', handleInput);
-CardForm.addEventListener('input', handleInput);
-
-
 const enableValidation = (valClasses) => {
-  
+  const UserForm = document.forms[valClasses.editUserId];
+  const CardForm = document.forms[valClasses.addCardID];  
+
+  // Для слушателя кнопки сохранения профиля
+  const profileFormSaveBtn = UserForm.querySelector(valClasses.saveFormClass);
+
+  // Для слушателя кнопки сохранения новой карты
+  const newCardSaveBtn = CardForm.querySelector(valClasses.saveFormClass);
+
+  UserForm.addEventListener('submit', (evt) => {
+    // profileFormSubmitHandler();
+    handleSubmit(evt, profileFormSubmitHandler);
+  });
+  CardForm.addEventListener('submit', (evt) => {    
+    handleSubmit(evt, addCardFormSubmitHandler);    
+  });
+
+  UserForm.addEventListener('input', (evt) => {
+    handleInput(evt, profileFormSaveBtn, valClasses.activeSubmitButtonClass);
+  });
+  CardForm.addEventListener('input', (evt) => {
+    handleInput(evt, newCardSaveBtn, valClasses.activeSubmitButtonClass);
+  });  
 };
 
 enableValidation(validClasses)
-
-
-// Названия классов, необходимые для валидации
-// validClasses = {
-//     formSelector: '.form',
-//     inputSelector: '.form__input',
-//     submitButtonSelector: '.form__save',
-//     activeButtonClass: 'form__save_active',
-//     inputErrorClass: 'form__input_error'
-//     // errorClass: 'popup__error_visible'
-// };
-
-// const showInputError = (formElement, inputElement, errorMessage) => {
-//     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//     inputElement.classList.add('form__input_type_error');
-//     errorElement.textContent = errorMessage;
-//     errorElement.classList.add('form__input-error_active');
-//   };
-
-//   const hideInputError = (formElement, inputElement) => {
-//     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-//     inputElement.classList.remove('form__input_type_error');
-//     errorElement.classList.remove('form__input-error_active');
-//     errorElement.textContent = '';
-//   };
-  
-//   const checkInputValidity = (formElement, inputElement) => {
-//     if (!inputElement.validity.valid) {
-//       showInputError(formElement, inputElement, inputElement.validationMessage);
-//     } else {
-//       hideInputError(formElement, inputElement);
-//     }
-//   };
-  
-//   const setEventListeners = (formElement) => {
-//     const inputList = Array.from(formElement.querySelectorAll('.form__input'));
-//     const buttonElement = formElement.querySelector('.form__submit')
-//     toggleButtonState(inputList, buttonElement);
-    
-//     inputList.forEach((inputElement) => {
-//       inputElement.addEventListener('input', function () {
-//         checkInputValidity(formElement, inputElement);
-//         toggleButtonState(inputList, buttonElement);
-//       });
-//     });
-//   };
-  
-//   const hasInvalidInput = (inputList) => {
-//     return inputList.some((inputElement) => {
-//       return !inputElement.validity.valid;
-//     });
-//   };
-  
-//   const toggleButtonState = (inputList, buttonElement) => {
-//     if (hasInvalidInput(inputList)) {
-//       buttonElement.classList.add('button_inactive')
-//       } else {
-//         buttonElement.classList.remove('button_inactive')
-//       }
-//     };
-
-
-
-// const enableValidation = (formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass) => {
-//     con
-//     const formList = Array.from(document.querySelectorAll('.form'));
-//     formList.forEach((formElement) => {
-//         formElement.addEventListener('submit', function (evt) {
-//         evt.preventDefault();
-//         });
-    
-//     const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
-      
-//     fieldsetList.forEach((formElement) => {
-//       setEventListeners(formElement);
-//     });    
-//   });
-
-//   }; 
-
-
-  
