0d2922c7e7a467b15e6d58f66fc16d7cfcf7f1c7
OlgMan2411 <omanaeva@yandex.ru>
2022-05-07 18:38:06 +0300

    Refactoring. Применены классы. К 1 превью

---
+++ scripts/constants/validClassesSelectors.js
@@ -0,0 +1,7 @@
+export default {
+    formSelector: ".form",
+    inputSelector: ".form__input",
+    submitButtonSelector: ".form__save",
+    activeSubmitButtonClass: "form__save_active",
+    inputErrorClass: "form__input_type-error",
+  };
